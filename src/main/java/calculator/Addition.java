d1363dd71a084fa8d93c74157cbe898def537133
Dominika <dominikaawiecek@gmail.com>
2017-09-04 00:29:27 +0200

    initial commit

---
+++ src/main/java/calculator/Addition.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Addition {
+
+    public double add(double a, double b) {
+        return a+b;
+    };
+
+}
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,123 @@
+package calculator;
+
+public class Calculator {
+
+    Integer result;
+
+    InputValues inputValues = new InputValues();
+    inputValues.setA(10);
+    inputValues.setB(14);
+
+    Addition addition;
+
+    result = addition.add(inputValues.getA(), inputValues.getB());
+
+
+    //
+//        public static void main(String[] args) {
+//            String nl = System.getProperty("line.separator");
+//            // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            Scanner in = new Scanner(System.in);
+//            int op;
+//            double x, y;
+//            double result;
+//            int x1, y1, result1;
+//
+//            System.out.println("Select operation:" + nl + "1.Addition" + nl + "2.Subtraction" + nl + "3.Multiplication" + nl + "4.Division" + nl + "5.Power" + nl + "6.Factorial" + nl + "7.Calculate percentage" + nl + "8.Nth root");
+//
+//            op = in.nextInt();
+//
+//            switch(op) {
+//                case 1:
+//                    System.out.println("You have chosen an addition " + nl + "Please enter the first number");
+//                    x = in.nextDouble();
+//                    System.out.println("Then enter the second number");
+//                    y = in.nextDouble();
+//
+//                    result = addition(x, y);
+//                    System.out.println("The result is " + result);
+//                    break;
+//
+//                case 2:
+//                    System.out.println("You have chosen a subtraction " + nl + "Please enter the first number");
+//                    x = in.nextDouble();
+//                    System.out.println("Then enter the second number");
+//                    y = in.nextDouble();
+//
+//                    result = subtraction(x,y);
+//                    System.out.println("The result is " + result);
+//                    break;
+//
+//                case 3:
+//                    System.out.println("You have chosen a multiplication" + nl + "Please, enter the first number");
+//                    x = in.nextDouble();
+//                    System.out.println("Then enter the second number");
+//                    y = in.nextDouble();
+//
+//                    result = multiplication(x,y);
+//                    System.out.println("The result is " + result);
+//                    break;
+//
+//                case 4:
+//                    System.out.println("You have chosen a division" + nl + "Please, enter the first number");
+//                    x = in.nextDouble();
+//                    System.out.println("Then enter the second number");
+//                    y = in.nextDouble();
+//
+//                    if ( y == 0 ) {
+//                        System.out.println("The operation can't be completed because denominator is equal 0!!!");
+//                    }
+//
+//                    else {
+//                        result = division(x,y);
+//                        System.out.println("The result is " + result);
+//                    }
+//                    break;
+//
+//                case 5:
+//                    System.out.println("You have chosen power" + nl + "Please, enter number you want raised to a power");
+//                    x1 = in.nextInt();
+//                    System.out.println("Enter the power itself");
+//                    y1 = in.nextInt();
+//
+//                    result1 = power(x1, y1);
+//                    System.out.println("The result is " + result1);
+//                    break;
+//
+//                case 6:
+//                    System.out.println("You have chosen factorial" + nl + "Please, enter the number you want to find the factorial");
+//                    x1 = in.nextInt();
+//
+//                    result1 = factorial(x1);
+//                    System.out.println("The result is " + result1);
+//                    break;
+//
+//                case 7:
+//                    System.out.println("You have chosen to calculate percentage" + nl + "Enter the percentage");
+//                    x = in.nextDouble();
+//                    System.out.println(x + "% of what number? Please select");
+//                    y = in.nextDouble();
+//
+//                    result = percentage(x, y);
+//                    System.out.println("The result is " + result);
+//                    break;
+//
+//                case 8:
+//                    System.out.println("You have chosen to calculate the nth root" +nl +"Enter the degree of the root" );
+//                    x1 = in.nextInt();
+//                    System.out.println("Enter number");
+//                    y1 = in.nextInt();
+//
+//
+//
+//                default:
+//                    System.out.println("No operation");
+//                    break;
+//            }
+//
+//
+//        }
+//
+//    }
+
+}
